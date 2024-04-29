23b392b62bdd1c80c2e0d96da359aa80835f5a25
Paulo Henrique <105134091+hulkdranski@users.noreply.github.com>
2024-04-29 11:50:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -39,73 +39,10 @@ document.addEventListener("DOMContentLoaded", () => {
       body: JSON.stringify(body),
     }
 
-    fetch("https://eo8dhklk17ufzkk.m.pipedream.net", options)
+    fetch("Your_pipedream_unique_URL_to_trigger_here", options)
 
     projectName.value = ""
     notes.value = ""
     
   });
 });
-
-/*const query = `mutation {
-  create_item (board_id: 6482815534, item_name: "kaue") {
-    id
-  }
-}`;
-
-
-
-// Fazendo a solicitação para a API do Monday.com
-fetch("https://api.monday.com/v2", {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjM0OTEzNzY3MywiYWFpIjoxMSwidWlkIjo1OTc1NjExMSwiaWFkIjoiMjAyNC0wNC0xOFQxODowMjo0OC4xNTVaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjI5NzM0MTgsInJnbiI6InVzZTEifQ.nFzc_Cehr0lUq9eyQEEQon0nOPmdPCA3B9QYxZjeess'
-  },
-  body: JSON.stringify({
-    'query': query
-  })
-})
-.then(res => res.json())
-.then(res => console.log(JSON.stringify(res, null, 2)))
-.catch(error => console.error("Erro ao fazer a solicitação:", error));*/ /*
-
-let query = 'query { webhooks (board_ids: 6482815534) { id event board_id config }}';
-
-fetch ("https://api.monday.com/v2", {
-  method: 'post',
-  headers: {
-    'Content-Type': 'application/json',
-   'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjM0OTEzNzY3MywiYWFpIjoxMSwidWlkIjo1OTc1NjExMSwiaWFkIjoiMjAyNC0wNC0xOFQxODowMjo0OC4xNTVaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjI5NzM0MTgsInJnbiI6InVzZTEifQ.nFzc_Cehr0lUq9eyQEEQon0nOPmdPCA3B9QYxZjeess'
-   },
-   body: JSON.stringify({
-     'query' : query
-   })
-  })
-   .then(res => res.json())
-   .then(res => console.log(JSON.stringify(res, null, 2)));
-
-   const columnValues = {
-    'text9': 'Valor de texto',
-    'project_status': 'Em andamento'
-  };*/
-  
-  const query = `mutation {
-    create_item (board_id: 6482815534, item_name: "Projeto-Teste", column_values: "{\\"text9\\": \\"Testando API\\", \\"project_status\\": \\"Em andamento\\"}") {
-      id
-    }
-  }`;
-  
-  fetch("https://api.monday.com/v2", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjM0OTEzNzY3MywiYWFpIjoxMSwidWlkIjo1OTc1NjExMSwiaWFkIjoiMjAyNC0wNC0xOFQxODowMjo0OC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjI5NzM0MTgsInJnbiI6InVzZTEifQ.hYRPkBf_C1gID3sL2Fvnf3WZM_L-YZxaX8ArnxUvvmU'
-    },
-    body: JSON.stringify({
-      'query': query
-    })
-  })
-  .then(res => res.json())
-  .then(res => console.log(JSON.stringify(res, null, 2)))
-  .catch(error => console.error("Erro ao fazer a solicitação:", error));
